ca7bca62d5d8c513faf6c1024ac5bebb3ed53d22
Hugo López-Fernández <hlfernandez@uvigo.es>
2022-06-06 20:59:42 +0200

    Supresses warning

--- src/main/java/es/uvigo/ei/sing/yacli/command/AbstractCommand.java
+++ src/main/java/es/uvigo/ei/sing/yacli/command/AbstractCommand.java
@@ -1,31 +1,33 @@
 package es.uvigo.ei.sing.yacli.command;
 
-import java.util.Collections;
+import static java.util.Collections.unmodifiableList;
+
 import java.util.List;
 
 import es.uvigo.ei.sing.yacli.command.option.Option;
 
 public abstract class AbstractCommand implements Command {
-	protected final List<Option<?>> options;
+  protected final List<Option<?>> options;
 
-	public AbstractCommand() {
-		this.options = createOptions();
-	}
+  public AbstractCommand() {
+    this.options = createOptions();
+  }
 
-	@Override
-	public List<Option<?>> getOptions() {
-		return Collections.unmodifiableList(options);
-	}
-	
-	@Override
-	public <T> Option<T> getOption(String name) {
-		for (Option<?> option : this.getOptions()) {
-			if (option.hasName(name))
-				return (Option<T>) option;
-		}
+  @Override
+  public List<Option<?>> getOptions() {
+    return unmodifiableList(options);
+  }
 
-		return null;
-	}
+  @SuppressWarnings("unchecked")
+  @Override
+  public Option<?> getOption(String name) {
+    for (Option<?> option : this.getOptions()) {
+      if (option.hasName(name))
+        return option;
+    }
 
-	protected abstract List<Option<?>> createOptions(); // factory method
+    return null;
+  }
+
+  protected abstract List<Option<?>> createOptions();
 }
